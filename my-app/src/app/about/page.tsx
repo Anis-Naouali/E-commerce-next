dc08c93b7aaa9ff362cb3b75ff61197356477cc6
mohamed-trabels <mohamed930trabelsi930@gmail.com>
2023-09-08 18:50:32 +0100

    about

--- my-app/src/app/about/page.tsx
+++ my-app/src/app/about/page.tsx
@@ -2,7 +2,25 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div>About page</div>
+    <div>
+      <div style={{marginLeft:"150px",marginTop:"80px"}}>
+      <div className="w-96 h-6 flex-col justify-start items-start gap-10 inline-flex">
+<div className="text-justify text-black text-5xl font-semibold leading-10 tracking-widest">Our Story</div>
+<div className="flex-col justify-start items-start gap-6 flex">
+<div className="w-96 text-black text-base font-normal leading-relaxed">Launced in 2015, Exclusive is South Asia’s premier online shopping makterplace with an active presense in Bangladesh. Supported by wide range of tailored marketing, data and service solutions, Exclusive has 10,500 sallers and 300 brands and serves 3 millioons customers across the region. </div>
+<div className="w-96 text-black text-base font-normal leading-relaxed">Exclusive has more than 1 Million products to offer, growing at a very fast. Exclusive offers a diverse assotment in categories ranging from consumer.</div>
+</div>
+</div>
+    </div>
+      <img style={{width:"500px", height:"500px", marginLeft:"1000px",marginTop:"-120px"}} src="tt.jpg" />
+      
+
+
+
+
+    
+    </div>
+    
   )
 }
 
